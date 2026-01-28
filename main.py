6f16084a67bdc46d0f9d4a1c9f12c4ec24e68306
haywik <nobodysgamer11@gmail.com>
2026-01-28 17:38:55 +0000

    Change error handling to redirect on bad path

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def index(request: Request): return RedirectResponse(url="/home")
 
 @app.get("/{path:path}")
 async def land(request: Request,path : str):
-    if os.path.exists("./templates/"+(path+"html")) == False: return tpl.TemplateResponse("bad_path.html", {"request":request}) 
+    if os.path.exists("./templates/"+(path+"html")) == False: return RedirectResponse(url="/bad_path")
     return tpl.TemplateResponse(file, {"request":request})
 
 
