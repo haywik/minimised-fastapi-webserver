6e83ab70b71fd0793b89fb0c4e91b4e08067d5ea
haywik <nobodysgamer11@gmail.com>
2026-01-28 18:00:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ async def index(request: Request): return RedirectResponse(url="/home")
 
 @app.get("/{path:path}")
 async def land(request: Request,path : str):
-    if os.path.exists("./templates/"+(path+"html")) == False: return RedirectResponse(url="/bad_path")
-    return tpl.TemplateResponse(file, {"request":request})
+    if os.path.exists("./templates/"+path1+".html") == False: return RedirectResponse(url="/bad_path")
+    return tpl.TemplateResponse((path1+".html"), {"request":request})
 
 
 
