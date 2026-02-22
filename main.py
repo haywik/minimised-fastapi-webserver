406de4574df263df44a29449367df15f56c80f77
haywik <nobodysgamer11@gmail.com>
2026-02-22 20:56:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #minimised version from central, 28.jan.2026
 #note: this is quite minimised tbf
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -17,7 +17,9 @@ async def index(request: Request): return RedirectResponse(url="/home")
 
 @app.get("/{path1:path}")
 async def land(request: Request,path1 : str):
-    if os.path.exists("__file__/templates/"+Path(path1).absolute()+".html")) == False or ".." in path1: return RedirectResponse(url="/bad_path")
+    if os.path.exists("__file__/templates/"+Path(path1).absolute()+".html")) == False or ".." in path1:
+        raise HTTPException(status_code=status.HTTP.404_NOT_FOUND)
+        return RedirectResponse(url="/bad_path")
     return tpl.TemplateResponse((path1+".html"), {"request":request})
 
 
