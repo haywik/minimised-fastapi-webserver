0cd6bea7fd6f908ece84c7a203189f78b12c8ca5
haywik <nobodysgamer11@gmail.com>
2026-02-28 16:08:03 +0000

    final

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 import re
+import os
 
 app = FastAPI()
 
@@ -18,16 +19,22 @@ app.mount("/static", StaticFiles(directory=str(base / "static")), name="static")
 async def index(request: Request):
     return RedirectResponse(url="/home")
 
+@app.get("/bad_path")
+async def bad_path(request: Request):
+    return tpl.TemplateResponse("bad_path.html", {"request": request}, status_code=404)
 
 @app.get("/{path1:path}")
 async def land(request: Request, path1: str):
     if not re.fullmatch(r'[a-zA-Z0-9_\-/]+', path1):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return RedirectResponse(url="/bad_path")
 
     resolved = (TEMPLATES_DIR / path1).resolve()
     file = resolved.with_suffix(".html")
 
     if not str(resolved).startswith(str(TEMPLATES_DIR.resolve())) or not file.exists():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return RedirectResponse(url="/bad_path")
 
-    return tpl.TemplateResponse(path1 + ".html", {"request": request})
+    if os.path.exists(str(Path(path1).resolve())+".html") == False:
+        return RedirectResponse(url="/bad_path")
+    else:
+    	return tpl.TemplateResponse(path1 + ".html", {"request": request})
